152203d7b8fb2a44e35e623507d673734cc1e3f2
Richard Dodd <richard.o.dodd@gmail.com>
2020-06-23 15:38:24 +0100

    Added dt

--- src/lib.rs
+++ src/lib.rs
@@ -5,23 +5,99 @@
 //!
 //! I'm also not implementing the non-stats stuff, such as graph drawing.
 
-use puruspe::{erf, gamma};
+mod puruspe;
+
+use puruspe::{betai, erf, gamma, inverf, ln_gamma};
 use std::f64::consts::PI;
 
 const FRAC_1_SQRT_2PI: f64 = 0.3989422804014327; // (2.0f64 * PI).sqrt().recip()
 const LN_SQRT_2PI: f64 = 0.9189385332046727; // (2.0 * PI).sqrt().ln()
 const LN_SQRT_PID2: f64 = 0.22579135264472733; // (0.5 * PI).sqrt().ln()
 
+macro_rules! r_d__0 {
+    ($log_p:expr) => {
+        if $log_p {
+            f64::NEG_INFINITY
+        } else {
+            0.0
+        }
+    };
+}
+
+macro_rules! r_d__1 {
+    ($log_p:expr) => {
+        if $log_p {
+            0.0
+        } else {
+            1.0
+        }
+    };
+}
+
+macro_rules! r_dt_0 {
+    ($lower_tail:expr, $log_p:expr) => {
+        if $lower_tail {
+            r_d__0!($log_p)
+        } else {
+            r_d__1!($log_p)
+        }
+    };
+}
+
+macro_rules! r_dt_1 {
+    ($lower_tail:expr, $log_p:expr) => {
+        if $lower_tail {
+            r_d__1!($log_p)
+        } else {
+            r_d__0!($log_p)
+        }
+    };
+}
+
+macro_rules! r_d_cval {
+    ($p:expr, $lower_tail:expr) => {
+        if $lower_tail {
+            0.5 - $p + 0.5
+        } else {
+            $p
+        }
+    };
+}
+
+/// Evaluate the probability density function of the normal distribution at x.
 pub fn dnorm(x: f64, mean: f64, sd: f64, log: bool) -> f64 {
-    (sd * FRAC_1_SQRT_2PI).recip() * (-0.5 * ((x - mean) / sd).powi(2)).exp()
+    let out = (sd * FRAC_1_SQRT_2PI).recip() * (-0.5 * ((x - mean) / sd).powi(2)).exp();
+    if log {
+        out.ln()
+    } else {
+        out
+    }
 }
 
+/// Evaluate the culmulative distribution function of the normal distribution at x.
 pub fn pnorm(q: f64, mean: f64, sd: f64, lower_tail: bool, log: bool) -> f64 {
-    0.5 * (1.0 + erf((q - mean) / (sd * 2.0f64.sqrt())))
+    let mut out = 0.5 * (1.0 + erf((q - mean) / (sd * 2.0f64.sqrt())));
+    if !lower_tail {
+        out = 1.0 - out;
+    }
+    if log {
+        out.ln()
+    } else {
+        out
+    }
 }
 
-pub fn qnorm(p: f64, mean: f64, sd: f64, lower_tail: bool, log: bool) -> f64 {
-    todo!()
+/// Evaluate the quantile function of the normal distribution at probability p.
+pub fn qnorm(mut p: f64, mean: f64, sd: f64, lower_tail: bool, log: bool) -> f64 {
+    if !lower_tail {
+        p = 1.0 - p;
+    }
+    let out = mean + sd * 2.0f64.sqrt() * inverf(2.0 * p - 1.0);
+    if log {
+        out.ln()
+    } else {
+        out
+    }
 }
 
 /// Compute the probability density function for the t distribution at `x`. `df` is the degrees of
@@ -67,22 +143,97 @@ pub fn dt(x: f64, df: f64, ncp: Option<f64>, log: bool) -> f64 {
 }
 
 /// Compute the culmulative distribution function (CDF) for the t distribution at `x`. `df` is the
-/// degrees of freedom and for hypothesis testing is the size of the sample - 1. `ncp`,
-/// `lower_tail` and `log_p` aren't implemented yet.
-pub fn pt(x: f64, df: f64, ncp: Option<f64>, lower_tail: bool, log_p: bool) -> f64 {
-    assert!(df > 0.0, "degrees of freedom must be > 0");
-    if ncp.is_some() || lower_tail || log_p {
-        todo!()
+/// degrees of freedom and for hypothesis testing is the size of the sample - 1.
+pub fn pt(x: f64, df: f64, lower_tail: bool, log_p: bool) -> f64 {
+    let n = df;
+    if n <= 0.0 || n.is_nan() || x.is_nan() {
+        return f64::NAN;
+    }
+    if x == f64::NEG_INFINITY {
+        return r_dt_0!(lower_tail, log_p);
+    } else if x == f64::INFINITY {
+        return r_dt_1!(lower_tail, log_p);
+    }
+    if n == f64::INFINITY {
+        return pnorm(x, 0.0, 1.0, lower_tail, log_p);
+    }
+    let nx = 1.0 + (x / n) * x;
+
+    let val = if nx > 1e100 {
+        let lval = -0.5 * n * (2.0 * x.abs().ln() - n.ln()) - lbeta(0.5 * n, 0.5) - (0.5 * n).ln();
+        if log_p {
+            lval
+        } else {
+            lval.exp()
+        }
+    } else {
+        if n > x * x {
+            pbeta(x * x / (n + x * x), 0.5, n * 0.5, false, log_p)
+        } else {
+            pbeta(nx.recip(), n * 0.5, 0.5, true, log_p)
+        }
+    };
+    let lower_tail = if x <= 0.0 { !lower_tail } else { lower_tail };
+    if log_p {
+        if lower_tail {
+            (-0.5f64 * val.exp()).ln_1p()
+        } else {
+            val - 2.0f64.ln()
+        }
+    } else {
+        r_d_cval!(val * 0.5, lower_tail)
+    }
+}
+
+/// Compute the quantile function (inverse CDF function) for the t distribution at `x`.
+pub fn qt(x: f64, df: f64, ncp: Option<f64>, lower_tail: bool, log_p: bool) -> f64 {
+    if df <= 0.0 {
+        return f64::NAN;
     }
     todo!()
 }
 
-/// Compute the quantile function (inverse CDF function) for the t distribution at `x`.
-pub fn qt(x: &[f64], df: f64, ncp: Option<f64>, lower_tail: bool, log_p: bool) -> f64 {
-    assert!(df > 0.0, "degrees of freedom must be > 0");
-    todo!()
+//dbeta todo
+/// Compute the probability density functino of the beta distribution at x.
+pub fn pbeta(x: f64, a: f64, b: f64, lower_tail: bool, log_p: bool) -> f64 {
+    if x.is_nan() || a.is_nan() || b.is_nan() {
+        x + a + b
+    } else if a < 0.0 || b < 0.0 {
+        f64::NAN
+    } else if x <= 0.0 {
+        r_dt_0!(lower_tail, log_p)
+    } else if x >= 1.0 {
+        r_dt_1!(lower_tail, log_p)
+    } else if a == 0.0 || b == 0.0 || !a.is_finite() || !b.is_finite() {
+        if a == 0.0 && b == 0.0 {
+            if log_p {
+                -2.0f64.ln()
+            } else {
+                0.5
+            }
+        } else if a == 0.0 || a / b == 0.0 {
+            r_dt_1!(lower_tail, log_p)
+        } else if b == 0.0 || b / a == 0.0 {
+            r_dt_0!(lower_tail, log_p)
+        } else if x < 0.5 {
+            r_dt_0!(lower_tail, log_p)
+        } else {
+            r_dt_1!(lower_tail, log_p)
+        }
+    } else {
+        let mut w = betai(a, b, x);
+        if !lower_tail {
+            w = 1.0 - w;
+        }
+        if log_p {
+            w = w.ln()
+        }
+        w
+    }
 }
+//qbeta todo
 
+/// Evaluates the "deviance part" from Catherine Loader's algorithm.
 fn bd0(x: f64, np: f64) -> f64 {
     if !x.is_finite() || !np.is_finite() || np == 0.0 {
         return f64::NAN;
@@ -124,7 +275,7 @@ fn stirlerr(n: f64) -> f64 {
     const S4: f64 = 0.0008417508417508417508417508; // 1/1188
 
     // error for 0, 0.5, 1.0, 1.5, ..., 14.5, 15.0.
-    const sferr_halves: &[f64] = &[
+    const SFERR_HALVES: &[f64] = &[
         0.0,                           /* n=0 - wrong, place holder only */
         0.1534264097200273452913848,   /* 0.5 */
         0.0810614667953272582196702,   /* 1.0 */
@@ -161,9 +312,9 @@ fn stirlerr(n: f64) -> f64 {
     if n <= 15.0 {
         let nn = n + n;
         if nn == nn.trunc() {
-            return sferr_halves[nn.trunc() as usize];
+            return SFERR_HALVES[nn.trunc() as usize];
         }
-        return lgammafn(n + 1.0, None) - (n + 0.5) * n.ln() + n - LN_SQRT_2PI;
+        return lgammafn(n + 1.0) - (n + 0.5) * n.ln() + n - LN_SQRT_2PI;
     }
 
     let nn = (n * n).recip();
@@ -179,17 +330,13 @@ fn stirlerr(n: f64) -> f64 {
     }
 }
 
-/// First return is the result of ln(gamma(x)). Second is the sign of the gamma function: true
-/// means positive and false means negative.
-fn lgammafn(x: f64, sgn: Option<&mut bool>) -> f64 {
+/// Returns the result of ln(gamma(x)).
+fn lgammafn(x: f64) -> f64 {
     const XMAX: f64 = 2.5327372760800758e+305; // f64::MAX / f64::MAX.ln()
     const DXREL: f64 = 1.4901161193847656e-08; // f64::EPSILON.sqrt()
 
     if x.is_nan() {
         return x;
-    }
-    if let Some(sgn) = sgn {
-        *sgn = !(x < 0.0 && (-x).floor() % 2.0 == 0.0);
     }
     if x <= 0.0 && x == x.trunc() {
         return f64::INFINITY;
@@ -215,13 +362,18 @@ fn lgammafn(x: f64, sgn: Option<&mut bool>) -> f64 {
         };
     }
     let sinpiy = sinpi(y).abs();
-    let ans = LN_SQRT_PID2;
+    let ans = LN_SQRT_PID2 + (x - 0.5) * y.ln() - x - sinpiy.ln() - lgammacor(y);
     if ((x - (x - 0.5).trunc()) * ans / x) < DXREL {
         //eprintln!("warning lgammafn precision low as x near 0.");
     }
     ans
 }
 
+/// Compute the log gamma correction factor for x >= 10 so that
+///
+/// `log(gamma(x)) = .5*log(2*pi) + (x-.5)*log(x) -x + lgammacor(x)`
+///
+/// `[ lgammacor(x) is called Del(x) in other contexts (e.g. dcdflib)]
 fn lgammacor(x: f64) -> f64 {
     const ALG_MCS: &[f64] = &[
         0.1666389480451863247205729650822e+0,
@@ -254,6 +406,138 @@ fn lgammacor(x: f64) -> f64 {
         chebyshev_eval(tmp * tmp * 2.0 - 1.0, &ALG_MCS[..NALGM]) / x
     } else {
         (x * 12.0).recip()
+    }
+}
+
+fn gammafn(x: f64) -> f64 {
+    const GMACS: &[f64] = &[
+        0.8571195590989331421920062399942e-2,
+        0.4415381324841006757191315771652e-2,
+        0.5685043681599363378632664588789e-1,
+        -0.4219835396418560501012500186624e-2,
+        0.1326808181212460220584006796352e-2,
+        -0.1893024529798880432523947023886e-3,
+        0.3606925327441245256578082217225e-4,
+        -0.6056761904460864218485548290365e-5,
+        0.1055829546302283344731823509093e-5,
+        -0.1811967365542384048291855891166e-6,
+        0.3117724964715322277790254593169e-7,
+        -0.5354219639019687140874081024347e-8,
+        0.9193275519859588946887786825940e-9,
+        -0.1577941280288339761767423273953e-9,
+        0.2707980622934954543266540433089e-10,
+        -0.4646818653825730144081661058933e-11,
+        0.7973350192007419656460767175359e-12,
+        -0.1368078209830916025799499172309e-12,
+        0.2347319486563800657233471771688e-13,
+        -0.4027432614949066932766570534699e-14,
+        0.6910051747372100912138336975257e-15,
+        -0.1185584500221992907052387126192e-15,
+        0.2034148542496373955201026051932e-16,
+        -0.3490054341717405849274012949108e-17,
+        0.5987993856485305567135051066026e-18,
+        -0.1027378057872228074490069778431e-18,
+        0.1762702816060529824942759660748e-19,
+        -0.3024320653735306260958772112042e-20,
+        0.5188914660218397839717833550506e-21,
+        -0.8902770842456576692449251601066e-22,
+        0.1527474068493342602274596891306e-22,
+        -0.2620731256187362900257328332799e-23,
+        0.4496464047830538670331046570666e-24,
+        -0.7714712731336877911703901525333e-25,
+        0.1323635453126044036486572714666e-25,
+        -0.2270999412942928816702313813333e-26,
+        0.3896418998003991449320816639999e-27,
+        -0.6685198115125953327792127999999e-28,
+        0.1146998663140024384347613866666e-28,
+        -0.1967938586345134677295103999999e-29,
+        0.3376448816585338090334890666666e-30,
+        -0.5793070335782135784625493333333e-31,
+    ];
+
+    const NGAM: usize = 22;
+    const XMIN: f64 = -170.5674972726612;
+    const XMAX: f64 = 171.61447887182298;
+    const XSML: f64 = 2.2474362225598545e-308;
+    const DXREL: f64 = 1.490116119384765696e-8;
+    if x.is_nan() {
+        return x;
+    }
+    if x == 0.0 || x < 0.0 && x == x.round() {
+        return f64::NAN;
+    }
+
+    let y = x.abs();
+    if y <= 10.0 {
+        let mut n = x as isize;
+        if x < 0.0 {
+            n -= 1;
+        }
+        let y = x - n as f64;
+        n -= 1;
+        let mut value = chebyshev_eval(y * 2.0 - 1.0, &GMACS[..NGAM]) + 0.9375;
+        if n == 0 {
+            return value;
+        } else if n < 0 {
+            if x < -0.5 && (x - (x - 0.5).floor() / x) < DXREL {
+                //panic!("less than 1/2 precision as near a pole (negative integer)
+            }
+            if y < XSML {
+                // overflow
+                if x > 0.0 {
+                    f64::INFINITY
+                } else {
+                    f64::NEG_INFINITY
+                }
+            } else {
+                let n = -n as usize;
+                for i in 0..n {
+                    value /= x + i as f64;
+                }
+                value
+            }
+        } else {
+            for i in 1..=n {
+                value *= y + i as f64;
+            }
+            value
+        }
+    } else {
+        if x > XMAX {
+            return f64::INFINITY;
+        } else if x < XMIN {
+            return 0.0;
+        }
+
+        let mut value;
+        if y <= 50.0 && y.floor() == y {
+            value = 1.0;
+            for i in 2..(y as usize) {
+                value *= i as f64;
+            }
+        } else {
+            let cor = if (2.0 * y).floor() == 2.0 * y {
+                stirlerr(y)
+            } else {
+                lgammacor(y)
+            };
+            value = ((y - 0.5) * y.ln() - y + LN_SQRT_2PI + cor).exp();
+        }
+
+        if x > 0.0 {
+            return value;
+        }
+
+        if (x - (x - 0.5).floor() / x).abs() < DXREL {
+            //println!("less than 0.5 precision")
+        }
+
+        let sinpiy = sinpi(y);
+        if sinpiy == 0.0 {
+            f64::INFINITY
+        } else {
+            -PI / (y * sinpiy * value)
+        }
     }
 }
 
@@ -297,5 +581,39 @@ fn sinpi(x: f64) -> f64 {
         -1.0
     } else {
         (PI * x).sin()
+    }
+}
+
+/// Computes ln(beta(a, b))
+fn lbeta(a: f64, b: f64) -> f64 {
+    if a.is_nan() || b.is_nan() {
+        return f64::NAN;
+    }
+    let p = a.min(b);
+    let q = a.max(b);
+
+    if p < 0.0 {
+        // also checks q >= 0
+        return f64::NAN;
+    } else if p == f64::INFINITY {
+        return f64::INFINITY;
+    } else if q == f64::INFINITY {
+        return f64::NEG_INFINITY;
+    }
+
+    if p >= 10.0 {
+        let corr = lgammacor(p) + lgammacor(q) - lgammacor(p + q);
+        q.ln() * -0.5
+            + LN_SQRT_2PI
+            + corr
+            + (p - 0.5) * (p / (p + q)).ln()
+            + q * (-p / (p + q)).ln_1p()
+    } else if q >= 10.0 {
+        let corr = lgammacor(q) - lgammacor(p + q);
+        lgammafn(p) + corr + p - p * (p + q).ln() + (q - 0.5) * (-p / (p + q)).ln_1p()
+    } else if p < 1e-306 {
+        ln_gamma(p) + (ln_gamma(q) - ln_gamma(p + q))
+    } else {
+        (gammafn(p) * (gammafn(q) / gammafn(p + q))).ln()
     }
 }
--- src/puruspe.rs
+++ src/puruspe.rs
@@ -1,5 +1,6 @@
-use std::f64::{EPSILON, MIN_POSITIVE};
+#![allow(dead_code)]
 use std::f64::consts::PI;
+use std::f64::{EPSILON, MIN_POSITIVE};
 
 // =============================================================================
 // Constants
@@ -11,38 +12,76 @@ const N: usize = 7;
 const ASWITCH: usize = 100;
 const NGAU: usize = 18;
 const Y: [f64; 18] = [
-    0.0021695375159141994, 0.011413521097787704, 0.027972308950302116,
-    0.051727015600492421, 0.082502225484340941, 0.12007019910960293,
-    0.16415283300752470, 0.21442376986779355, 0.27051082840644336, 
-    0.33199876341447887, 0.39843234186401943, 0.46931971407375483, 
-    0.54413605556657973, 0.62232745288031077, 0.70331500465597174, 
-    0.78649910768313447, 0.87126389619061517, 0.95698180152629142
+    0.0021695375159141994,
+    0.011413521097787704,
+    0.027972308950302116,
+    0.051727015600492421,
+    0.082502225484340941,
+    0.12007019910960293,
+    0.16415283300752470,
+    0.21442376986779355,
+    0.27051082840644336,
+    0.33199876341447887,
+    0.39843234186401943,
+    0.46931971407375483,
+    0.54413605556657973,
+    0.62232745288031077,
+    0.70331500465597174,
+    0.78649910768313447,
+    0.87126389619061517,
+    0.95698180152629142,
 ];
 const W: [f64; 18] = [
-    0.0055657196642445571, 0.012915947284065419, 0.020181515297735382,
-    0.027298621498568734, 0.034213810770299537, 0.040875750923643261,
-    0.047235083490265582, 0.053244713977759692, 0.058860144245324798,
-    0.064039797355015485, 0.068745323835736408, 0.072941885005653087,
-    0.076598410645870640, 0.079687828912071670, 0.082187266704339706,
-    0.084078218979661945, 0.085346685739338721, 0.085983275670394821
+    0.0055657196642445571,
+    0.012915947284065419,
+    0.020181515297735382,
+    0.027298621498568734,
+    0.034213810770299537,
+    0.040875750923643261,
+    0.047235083490265582,
+    0.053244713977759692,
+    0.058860144245324798,
+    0.064039797355015485,
+    0.068745323835736408,
+    0.072941885005653087,
+    0.076598410645870640,
+    0.079687828912071670,
+    0.082187266704339706,
+    0.084078218979661945,
+    0.085346685739338721,
+    0.085983275670394821,
 ];
 // Error functions
 const NCOEF: usize = 28;
 const COF: [f64; 28] = [
-    -1.3026537197817094, 6.4196979235649026e-1,
-    1.9476473204185836e-2, -9.561514786808631e-3,
-    -9.46595344482036e-4, 3.66839497852761e-4,
-    4.2523324806907e-5, -2.0278578112534e-5,
-    -1.624290004647e-6, 1.303655835580e-6,
-    1.5626441722e-8, -8.5238095915e-8,
-    6.529054439e-9, 5.059343495e-9,
-    -9.91364156e-10, -2.27365122e-10,
-    9.6467911e-11, 2.394038e-12,
-    -6.886027e-12, 8.94487e-13,
-    3.13092e-13, -1.12708e-13,
-    3.81e-16, 7.106e-15,
-    -1.523e-15, -9.4e-17,
-    1.21e-16,-2.8e-17
+    -1.3026537197817094,
+    6.4196979235649026e-1,
+    1.9476473204185836e-2,
+    -9.561514786808631e-3,
+    -9.46595344482036e-4,
+    3.66839497852761e-4,
+    4.2523324806907e-5,
+    -2.0278578112534e-5,
+    -1.624290004647e-6,
+    1.303655835580e-6,
+    1.5626441722e-8,
+    -8.5238095915e-8,
+    6.529054439e-9,
+    5.059343495e-9,
+    -9.91364156e-10,
+    -2.27365122e-10,
+    9.6467911e-11,
+    2.394038e-12,
+    -6.886027e-12,
+    8.94487e-13,
+    3.13092e-13,
+    -1.12708e-13,
+    3.81e-16,
+    7.106e-15,
+    -1.523e-15,
+    -9.4e-17,
+    1.21e-16,
+    -2.8e-17,
 ];
 // Incomplete beta function
 const SWITCH: usize = 3000;
@@ -57,13 +96,13 @@ pub fn gammp(a: f64, x: f64) -> f64 {
         0f64
     } else if (a as usize) >= ASWITCH {
         // Quadrature
-        gammpapprox(a,x,IncGamma::P)
+        gammpapprox(a, x, IncGamma::P)
     } else if x < a + 1f64 {
         // Series representation
-        gser(a,x)
+        gser(a, x)
     } else {
         // Continued fraction representation
-        1f64 - gcf(a,x)
+        1f64 - gcf(a, x)
     }
 }
 
@@ -74,13 +113,13 @@ pub fn gammq(a: f64, x: f64) -> f64 {
         1f64
     } else if (a as usize) >= ASWITCH {
         // Quadrature
-        gammpapprox(a,x,IncGamma::Q)
+        gammpapprox(a, x, IncGamma::Q)
     } else if x < a + 1f64 {
         // Series representation
-        1f64 - gser(a,x)
+        1f64 - gser(a, x)
     } else {
         // Continued fraction representation
-        gcf(a,x)
+        gcf(a, x)
     }
 }
 
@@ -92,7 +131,7 @@ fn gser(a: f64, x: f64) -> f64 {
     let mut sum = 1f64 / a;
     loop {
         ap += 1f64;
-        del *= x/ap;
+        del *= x / ap;
         sum += del;
         if del.abs() < sum.abs() * EPS {
             return sum * (-x + a * x.ln() - gln).exp();
@@ -108,10 +147,10 @@ fn gcf(a: f64, x: f64) -> f64 {
     let mut d = 1f64 / b;
     let mut h = d;
     let mut an: f64;
-    for i in 1 .. {
+    for i in 1.. {
         an = -i as f64 * (i as f64 - a);
         b += 2f64;
-        d = an*d + b;
+        d = an * d + b;
         if d.abs() < FPMIN {
             d = FPMIN;
         }
@@ -133,7 +172,7 @@ fn gcf(a: f64, x: f64) -> f64 {
 #[derive(Debug, Copy, Clone)]
 enum IncGamma {
     P,
-    Q
+    Q,
 }
 
 /// Gauss Legendre Quadrature (order of 18)
@@ -149,9 +188,9 @@ fn gammpapprox(a: f64, x: f64, psig: IncGamma) -> f64 {
     };
     let mut sum = 0f64;
     let mut t: f64;
-    for j in 0 .. NGAU {
+    for j in 0..NGAU {
         t = x + (xu - x) * Y[j];
-        sum += W[j] * (-(t-a1) + a1*(t.ln() - lna1)).exp();
+        sum += W[j] * (-(t - a1) + a1 * (t.ln() - lna1)).exp();
     }
     let ans = sum * (xu - x) * (a1 * (lna1 - 1f64).exp() - gln);
     match psig {
@@ -193,8 +232,8 @@ pub fn invgammp(p: f64, a: f64) -> f64 {
         afac = (a1 * (lna1 - 1f64) - gln).exp();
         pp = if p < 0.5 { p } else { 1f64 - p };
         t = (-2f64 * pp.ln()).sqrt();
-        let mut x = (2.30753 + t * 0.27061)/(1f64 + t * (0.99229 + t * 0.04481)) - t;
-        if p < 0.5 { 
+        let mut x = (2.30753 + t * 0.27061) / (1f64 + t * (0.99229 + t * 0.04481)) - t;
+        if p < 0.5 {
             x = -x;
         }
         1e-3_f64.max(a * (1f64 - 1f64 / (9f64 * a) - x / (3f64 * a.sqrt())).powi(3))
@@ -207,7 +246,7 @@ pub fn invgammp(p: f64, a: f64) -> f64 {
         }
     };
 
-    for _j in 0 .. 12 {
+    for _j in 0..12 {
         // x is too small to compute accurately
         if x <= 0f64 {
             return 0f64;
@@ -243,15 +282,15 @@ const LG5N7: [f64; 7] = [
     24.01409824118972,
     -1.2317395783752254,
     0.0012086577526594748,
-    -0.00000539702438713199
+    -0.00000539702438713199,
 ];
 
 /// Logarithm Gamma
-fn ln_gamma(z: f64) -> f64 {
+pub fn ln_gamma(z: f64) -> f64 {
     let z = z - 1f64;
     let base = z + G + 0.5;
     let mut s = 0f64;
-    for i in 1 .. N {
+    for i in 1..N {
         s += LG5N7[i] / (z + i as f64);
     }
     s += LG5N7[0];
@@ -263,7 +302,7 @@ pub fn gamma(z: f64) -> f64 {
     if z > 1f64 {
         let z_int = z as usize;
         if z - (z_int as f64) == 0f64 {
-            return factorial(z_int-1) as f64;
+            return factorial(z_int - 1) as f64;
         }
     }
 
@@ -279,9 +318,8 @@ pub fn gamma(z: f64) -> f64 {
 // =============================================================================
 /// Beta function
 pub fn beta(z: f64, w: f64) -> f64 {
-    (ln_gamma(z) + ln_gamma(w) - ln_gamma(z+w)).exp()
+    (ln_gamma(z) + ln_gamma(w) - ln_gamma(z + w)).exp()
 }
-
 
 // =============================================================================
 // Error functions
@@ -312,7 +350,7 @@ fn erfccheb(z: f64) -> f64 {
     assert!(z >= 0f64, "erfccheb requires nonnegative argument");
     let t = 2f64 / (2f64 + z);
     let ty = 4f64 * t - 2f64;
-    for j in (1 .. NCOEF-1).rev() {
+    for j in (1..NCOEF - 1).rev() {
         let tmp = d;
         d = ty * d - dd + COF[j];
         dd = tmp;
@@ -332,7 +370,7 @@ pub fn inverfc(p: f64) -> f64 {
     let pp = if p < 1f64 { p } else { 2f64 - p };
     let t = (-2f64 * (pp / 2f64).ln()).sqrt();
     let mut x = -0.70711 * ((2.30753 + t * 0.27061) / (1f64 + t * (0.99229 + t * 0.04481)) - t);
-    for _j in 0 .. 2 {
+    for _j in 0..2 {
         let err = erfc(x) - pp;
         x += err / (1.12837916709551257 * (-x.powi(2)).exp() - x * err);
     }
@@ -361,7 +399,7 @@ pub fn betai(a: f64, b: f64, x: f64) -> f64 {
         return betaiapprox(a, b, x);
     }
     let bt = (ln_gamma(a + b) - ln_gamma(a) - ln_gamma(b) + a * x.ln() + b * (1f64 - x).ln()).exp();
-    if x < (a + 1f64) / (a + b*2f64) {
+    if x < (a + 1f64) / (a + b * 2f64) {
         bt * betacf(a, b, x) / a
     } else {
         1f64 - bt * betacf(b, a, 1f64 - x) / b
@@ -380,7 +418,7 @@ fn betacf(a: f64, b: f64, x: f64) -> f64 {
     }
     d = 1f64 / d;
     let mut h = d;
-    for m in 1 .. 10000 {
+    for m in 1..10000 {
         let m = m as f64;
         let m2 = 2f64 * m;
         let mut aa = m * (b - m) * x / ((qam + m2) * (a + m2));
@@ -422,18 +460,24 @@ fn betaiapprox(a: f64, b: f64, x: f64) -> f64 {
     let lnmuc = (1f64 - mu).ln();
     let mut t = (a * b / ((a + b).powi(2) * (a + b + 1f64))).sqrt();
     let xu = if x > a / (a + b) {
-        if x >= 1f64 { return 1f64; }
+        if x >= 1f64 {
+            return 1f64;
+        }
         1f64.min((mu + 10f64 * t).max(x + 5f64 * t))
     } else {
-        if x <= 0f64 { return 0f64; }
+        if x <= 0f64 {
+            return 0f64;
+        }
         0f64.max((mu - 10f64 * t).min(x - 5f64 * t))
     };
     let mut sum = 0f64;
-    for j in 0 .. 18 {
+    for j in 0..18 {
         t = x + (xu - x) * Y[j];
         sum += W[j] * (a1 * (t.ln() - lnmu) + b1 * (1f64 - t).ln() - lnmuc).exp();
     }
-    let ans = sum * (xu - x) * (a1 * lnmu - ln_gamma(a) + b1 * lnmuc - ln_gamma(b) + ln_gamma(a + b)).exp();
+    let ans = sum
+        * (xu - x)
+        * (a1 * lnmu - ln_gamma(a) + b1 * lnmuc - ln_gamma(b) + ln_gamma(a + b)).exp();
     if ans > 0f64 {
         1f64 - ans
     } else {
@@ -447,7 +491,7 @@ pub fn invbetai(p: f64, a: f64, b: f64) -> f64 {
     let mut t: f64;
     let mut x: f64;
     let mut u: f64;
-    if p <= 0f64 { 
+    if p <= 0f64 {
         return 0f64;
     } else if p >= 1f64 {
         return 1f64;
@@ -455,10 +499,14 @@ pub fn invbetai(p: f64, a: f64, b: f64) -> f64 {
         let pp = if p < 0.5 { p } else { 1f64 - p };
         t = (-2f64 * pp.ln()).sqrt();
         x = (2.30753 + t * 0.27061) / (1f64 + t * (0.99229 + t * 0.04481)) - t;
-        if p < 0.5 { x = -x; }
+        if p < 0.5 {
+            x = -x;
+        }
         let al = (x.powi(2) - 3f64) / 6f64;
         let h = 2f64 / (1f64 / (2f64 * a - 1f64) + 1f64 / (2f64 * b - 1f64));
-        let w = (x * (al + h).sqrt() / h) - (1f64 / (2f64 * b - 1f64) - 1f64 / (2f64 * a - 1f64)) * (al + 5f64 / 6f64 - 2f64 / (3f64 * h));
+        let w = (x * (al + h).sqrt() / h)
+            - (1f64 / (2f64 * b - 1f64) - 1f64 / (2f64 * a - 1f64))
+                * (al + 5f64 / 6f64 - 2f64 / (3f64 * h));
         x = a / (a + b * (2f64 * w).exp());
     } else {
         let lna = (a / (a + b)).ln();
@@ -472,8 +520,8 @@ pub fn invbetai(p: f64, a: f64, b: f64) -> f64 {
             1f64 - (b * w * (1f64 - p)).powf(1f64 / b)
         };
     }
-    let afac = - ln_gamma(a) - ln_gamma(b) + ln_gamma(a + b);
-    for j in 0 .. 10 {
+    let afac = -ln_gamma(a) - ln_gamma(b) + ln_gamma(a + b);
+    for j in 0..10 {
         if x == 0f64 || x == 1f64 {
             return x;
         }
