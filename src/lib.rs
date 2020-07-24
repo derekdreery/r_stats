282da39d4b193db5ed32e4def89c253a27c11008
Richard Dodd <richard.o.dodd@gmail.com>
2020-07-24 22:13:19 +0100

    Add up to (and inc) exponential distribution.

--- src/lib.rs
+++ src/lib.rs
@@ -29,7 +29,45 @@ pub mod ffi {
 // Normal distribution
 
 /// Evaluate the probability density function of the normal distribution with mean `mu` and
-/// variance `sigma` at `x`.
+/// variance `sigma` squared at `x`.
+///
+/// The formula for this function is:
+///
+/// <math>
+///   <mfrac>
+///     <mrow>
+///       <mn>1</mn>
+///     </mrow>
+///     <mrow>
+///       <mi>&sigma;</mi>
+///       <msqrt>
+///         <mn>2</mn>
+///         <!-- <mo>&InvisibleTimes;</mo> -- this breaks layout -->
+///         <mi>&pi;</mi>
+///       </msqrt>
+///     </mrow>
+///   </mfrac>
+///   <msup>
+///     <mn>e</mn>
+///     <mrow>
+///       <mo>-</mo>
+///       <mfrac>
+///         <mn>1</mn>
+///         <mn>2</mn>
+///       </mfrac>
+///       <mfenced open="(" close=")">
+///         <mfrac>
+///           <mrow>
+///             <mi>x</mi>
+///             <mo>-</mo>
+///             <mn>&mu;</mn>
+///           </mrow>
+///           <mn>&sigma;</mn>
+///         </mfrac>
+///       </mfenced>
+///     </mrow>
+///   </msup>
+/// </math>
 ///
 /// If `give_log` is true, the natural logarithm of the value will be returned, with potentially
 /// higher numerical accuracy than calling `.ln()` on the result.
@@ -38,7 +76,7 @@ pub fn normal_pdf(x: f64, mu: f64, sigma: f64, give_log: bool) -> f64 {
 }
 
 /// Evaluate the culmulative density function of the normal distribution with mean `mu` and
-/// variance `sigma` at `x`.
+/// variance `sigma` squared at `x`.
 ///
 /// If `lower_tail` is true, the integral from `-∞` to `x` is evaluated, else the
 /// integral from `x` to `∞` is evaluated instead. "Usual" behaviour corresponds to
@@ -52,7 +90,7 @@ pub fn normal_cdf(x: f64, mu: f64, sigma: f64, lower_tail: bool, log_p: bool) ->
 }
 
 /// Evaluate the quantile function of the normal distribution with mean `mu` and
-/// variance `sigma` at probability `p`.
+/// variance `sigma` squared at probability `p`.
 ///
 /// If `lower_tail` is true, then `p` is the integral from `∞` to `x`, else it is the integral
 /// from `x` to `∞`. "Usual" behaviour corresponds to `true`.
@@ -183,6 +221,7 @@ pub fn beta_cdf(x: f64, a: f64, b: f64, lower_tail: bool, log_p: bool) -> f64 {
 pub fn beta_quantile(p: f64, a: f64, b: f64, lower_tail: bool, log_p: bool) -> f64 {
     unsafe { ffi::qbeta(p, a, b, c_bool(lower_tail), c_bool(log_p)) }
 }
+
 // Log-normal distribution
 
 /// Evaluate the probability density function of the log-normal distribution with parameters
@@ -225,6 +264,276 @@ pub fn lognormal_quantile(
 ) -> f64 {
     unsafe { ffi::qlnorm(p, mean_log, sd_log, c_bool(lower_tail), c_bool(log_p)) }
 }
+
+// Chi-squared distribution
+
+/// Evaluate the probability density function of the chi-squared distribution with `df` degrees of
+/// freedom at `x`.
+///
+/// If `give_log` is true, the natural logarithm of the value will be returned, with potentially
+/// higher numerical accuracy than calling `.ln()` on the result.
+pub fn chi_squared_pdf(x: f64, df: f64, give_log: bool) -> f64 {
+    unsafe { ffi::dchisq(x, df, c_bool(give_log)) }
+}
+
+/// Evaluate the culmulative distribution function of the chi-squared distribution with `df`
+/// degrees of freedom at `x`.
+///
+/// If `lower_tail` is true, the integral from `-∞` to `x` is evaluated, else the
+/// integral from `x` to `∞` is evaluated instead. "Usual" behaviour corresponds to
+/// `true`. Using `lower_tail = false` gives higher numerical accuracy than performing the
+/// calculation `1 - result` on `lower_tail = true` (when the result is close to 1).
+///
+/// If `log_p` is true, the natural logarithm of the value will be returned, with potentially
+/// higher numerical accuracy than calling `.ln()` on the result.
+pub fn chi_squared_cdf(x: f64, df: f64, lower_tail: bool, log_p: bool) -> f64 {
+    unsafe { ffi::pchisq(x, df, c_bool(lower_tail), c_bool(log_p)) }
+}
+
+/// Evaluate the quantile function of the chi-squared distribution with `df` degrees of freedom at
+/// probability `p`.
+///
+/// If `lower_tail` is true, then `p` is the integral from `∞` to `x`, else it is the integral
+/// from `x` to `∞`. "Usual" behaviour corresponds to `true`.
+///
+/// If `log_p` is true, the natural logarithm of the value will be returned, with potentially
+/// higher numerical accuracy than calling `.ln()`.
+pub fn chi_squared_quantile(p: f64, df: f64, lower_tail: bool, log_p: bool) -> f64 {
+    unsafe { ffi::qchisq(p, df, c_bool(lower_tail), c_bool(log_p)) }
+}
+
+// Non-central chi-squared distribution
+
+/// Evaluate the probability density function of the non-central chi-squared distribution with `df`
+/// degrees of freedom and non-centrality parameter `ncp` at `x`.
+///
+/// If `give_log` is true, the natural logarithm of the value will be returned, with potentially
+/// higher numerical accuracy than calling `.ln()` on the result.
+pub fn noncentral_chi_squared_pdf(x: f64, df: f64, ncp: f64, give_log: bool) -> f64 {
+    unsafe { ffi::dnchisq(x, df, ncp, c_bool(give_log)) }
+}
+
+/// Evaluate the culmulative distribution function of the non-central chi-squared distribution with
+/// `df` degrees of freedom and non-centrality parameter `ncp` at `x`.
+///
+/// If `lower_tail` is true, the integral from `-∞` to `x` is evaluated, else the
+/// integral from `x` to `∞` is evaluated instead. "Usual" behaviour corresponds to
+/// `true`. Using `lower_tail = false` gives higher numerical accuracy than performing the
+/// calculation `1 - result` on `lower_tail = true` (when the result is close to 1).
+///
+/// If `log_p` is true, the natural logarithm of the value will be returned, with potentially
+/// higher numerical accuracy than calling `.ln()` on the result.
+pub fn noncentral_chi_squared_cdf(x: f64, df: f64, ncp: f64, lower_tail: bool, log_p: bool) -> f64 {
+    unsafe { ffi::pnchisq(x, df, ncp, c_bool(lower_tail), c_bool(log_p)) }
+}
+
+/// Evaluate the quantile function of the non-central chi-squared distribution with `df` degrees of
+/// freedom and non-centrality parameter `ncp` at probability `p`.
+///
+/// If `lower_tail` is true, then `p` is the integral from `∞` to `x`, else it is the integral
+/// from `x` to `∞`. "Usual" behaviour corresponds to `true`.
+///
+/// If `log_p` is true, the natural logarithm of the value will be returned, with potentially
+/// higher numerical accuracy than calling `.ln()`.
+pub fn noncentral_chi_squared_quantile(
+    p: f64,
+    df: f64,
+    ncp: f64,
+    lower_tail: bool,
+    log_p: bool,
+) -> f64 {
+    unsafe { ffi::qnchisq(p, df, ncp, c_bool(lower_tail), c_bool(log_p)) }
+}
+
+// F distribution
+
+/// Evaluate the probability density function of the f distribution with parameters `df1` and `df2`
+/// at `x`. TODO I think df1 is the numerator degrees of freedom (when viewed as the ratio of two
+/// chi-squared dists. Check and doc this.
+///
+/// If `give_log` is true, the natural logarithm of the value will be returned, with potentially
+/// higher numerical accuracy than calling `.ln()` on the result.
+pub fn f_pdf(x: f64, df1: f64, df2: f64, give_log: bool) -> f64 {
+    unsafe { ffi::df(x, df1, df2, c_bool(give_log)) }
+}
+
+/// Evaluate the culmulative distribution function of the f distribution with parameters `df1` and
+/// `df2` at `x`.
+///
+/// If `lower_tail` is true, the integral from `-∞` to `x` is evaluated, else the
+/// integral from `x` to `∞` is evaluated instead. "Usual" behaviour corresponds to
+/// `true`. Using `lower_tail = false` gives higher numerical accuracy than performing the
+/// calculation `1 - result` on `lower_tail = true` (when the result is close to 1).
+///
+/// If `log_p` is true, the natural logarithm of the value will be returned, with potentially
+/// higher numerical accuracy than calling `.ln()` on the result.
+pub fn f_cdf(x: f64, df1: f64, df2: f64, lower_tail: bool, log_p: bool) -> f64 {
+    unsafe { ffi::pf(x, df1, df2, c_bool(lower_tail), c_bool(log_p)) }
+}
+
+/// Evaluate the quantile function of the f distribution with parameters `df1` and `df2` at
+/// probability `p`.
+///
+/// If `lower_tail` is true, then `p` is the integral from `∞` to `x`, else it is the integral
+/// from `x` to `∞`. "Usual" behaviour corresponds to `true`.
+///
+/// If `log_p` is true, the natural logarithm of the value will be returned, with potentially
+/// higher numerical accuracy than calling `.ln()`.
+pub fn f_quantile(p: f64, df1: f64, df2: f64, lower_tail: bool, log_p: bool) -> f64 {
+    unsafe { ffi::qf(p, df1, df2, c_bool(lower_tail), c_bool(log_p)) }
+}
+
+// Student's t distribution
+
+/// Evaluate the probability density function of the student's t distribution with degrees of
+/// freedom `df` at `x`.
+///
+/// If `give_log` is true, the natural logarithm of the value will be returned, with potentially
+/// higher numerical accuracy than calling `.ln()` on the result.
+pub fn students_t_pdf(x: f64, df: f64, give_log: bool) -> f64 {
+    unsafe { ffi::dt(x, df, c_bool(give_log)) }
+}
+
+/// Evaluate the culmulative distribution function of the student's t distribution with degrees of
+/// freedom `df` at `x`.
+///
+/// If `lower_tail` is true, the integral from `-∞` to `x` is evaluated, else the
+/// integral from `x` to `∞` is evaluated instead. "Usual" behaviour corresponds to
+/// `true`. Using `lower_tail = false` gives higher numerical accuracy than performing the
+/// calculation `1 - result` on `lower_tail = true` (when the result is close to 1).
+///
+/// If `log_p` is true, the natural logarithm of the value will be returned, with potentially
+/// higher numerical accuracy than calling `.ln()` on the result.
+pub fn students_t_cdf(x: f64, df: f64, lower_tail: bool, log_p: bool) -> f64 {
+    unsafe { ffi::pt(x, df, c_bool(lower_tail), c_bool(log_p)) }
+}
+
+/// Evaluate the quantile function of the student's t distribution with degrees of
+/// freedom `df` at probability `p`.
+///
+/// If `lower_tail` is true, then `p` is the integral from `∞` to `x`, else it is the integral
+/// from `x` to `∞`. "Usual" behaviour corresponds to `true`.
+///
+/// If `log_p` is true, the natural logarithm of the value will be returned, with potentially
+/// higher numerical accuracy than calling `.ln()`.
+pub fn students_t_quantile(p: f64, df: f64, lower_tail: bool, log_p: bool) -> f64 {
+    unsafe { ffi::qt(p, df, c_bool(lower_tail), c_bool(log_p)) }
+}
+
+// Binomial distribution
+
+/// Evaluate the probability density function of the binomial distribution with `n` trials and
+/// probability of success `p` at `x`.
+///
+/// If `give_log` is true, the natural logarithm of the value will be returned, with potentially
+/// higher numerical accuracy than calling `.ln()` on the result.
+pub fn binomial_pdf(x: f64, n: f64, p: f64, give_log: bool) -> f64 {
+    unsafe { ffi::dbinom(x, n, p, c_bool(give_log)) }
+}
+
+/// Evaluate the culmulative distribution function of the binomial distribution with `n` trials and
+/// probability of success `p` at `x`.
+///
+/// If `lower_tail` is true, the integral from `-∞` to `x` is evaluated, else the
+/// integral from `x` to `∞` is evaluated instead. "Usual" behaviour corresponds to
+/// `true`. Using `lower_tail = false` gives higher numerical accuracy than performing the
+/// calculation `1 - result` on `lower_tail = true` (when the result is close to 1).
+///
+/// If `log_p` is true, the natural logarithm of the value will be returned, with potentially
+/// higher numerical accuracy than calling `.ln()` on the result.
+pub fn binomial_cdf(x: f64, n: f64, p: f64, lower_tail: bool, log_p: bool) -> f64 {
+    unsafe { ffi::pbinom(x, n, p, c_bool(lower_tail), c_bool(log_p)) }
+}
+
+/// Evaluate the quantile function of the binomial distribution with `n` trials and
+/// probability of success `pr` at probability `p`.
+///
+/// If `lower_tail` is true, then `p` is the integral from `∞` to `x`, else it is the integral
+/// from `x` to `∞`. "Usual" behaviour corresponds to `true`.
+///
+/// If `log_p` is true, the natural logarithm of the value will be returned, with potentially
+/// higher numerical accuracy than calling `.ln()`.
+pub fn binomial_quantile(p: f64, n: f64, pr: f64, lower_tail: bool, log_p: bool) -> f64 {
+    unsafe { ffi::qbinom(p, n, pr, c_bool(lower_tail), c_bool(log_p)) }
+}
+
+// Ignoring multnomial because it only has random generation (and is spelt wrong - I bet it isn't
+// used much).
+
+// Cauchy distribution
+
+/// Evaluate the probability density function of the Cauchy distribution with parameters `location`
+/// and `scale` at `x`.
+///
+/// If `give_log` is true, the natural logarithm of the value will be returned, with potentially
+/// higher numerical accuracy than calling `.ln()` on the result.
+pub fn cauchy_pdf(x: f64, location: f64, scale: f64, give_log: bool) -> f64 {
+    unsafe { ffi::dbinom(x, location, scale, c_bool(give_log)) }
+}
+
+/// Evaluate the culmulative distribution function of the Cauchy distribution with parameters
+/// `location` and `scale` at `x`.
+///
+/// If `lower_tail` is true, the integral from `-∞` to `x` is evaluated, else the
+/// integral from `x` to `∞` is evaluated instead. "Usual" behaviour corresponds to
+/// `true`. Using `lower_tail = false` gives higher numerical accuracy than performing the
+/// calculation `1 - result` on `lower_tail = true` (when the result is close to 1).
+///
+/// If `log_p` is true, the natural logarithm of the value will be returned, with potentially
+/// higher numerical accuracy than calling `.ln()` on the result.
+pub fn cauchy_cdf(x: f64, location: f64, scale: f64, lower_tail: bool, log_p: bool) -> f64 {
+    unsafe { ffi::pbinom(x, location, scale, c_bool(lower_tail), c_bool(log_p)) }
+}
+
+/// Evaluate the quantile function of the Cauchy distribution with parameters
+/// `location` and `scale` at probability `p`.
+///
+/// If `lower_tail` is true, then `p` is the integral from `∞` to `x`, else it is the integral
+/// from `x` to `∞`. "Usual" behaviour corresponds to `true`.
+///
+/// If `log_p` is true, the natural logarithm of the value will be returned, with potentially
+/// higher numerical accuracy than calling `.ln()`.
+pub fn cauchy_quantile(p: f64, location: f64, scale: f64, lower_tail: bool, log_p: bool) -> f64 {
+    unsafe { ffi::qbinom(p, location, scale, c_bool(lower_tail), c_bool(log_p)) }
+}
+
+// Exponential distribution
+
+/// Evaluate the probability density function of the exponential distribution with given `scale` at
+/// `x`.
+///
+/// If `give_log` is true, the natural logarithm of the value will be returned, with potentially
+/// higher numerical accuracy than calling `.ln()` on the result.
+pub fn exponential_pdf(x: f64, scale: f64, give_log: bool) -> f64 {
+    unsafe { ffi::dbinom(x, scale, c_bool(give_log)) }
+}
+
+/// Evaluate the culmulative distribution function of the exponential distribution with given
+/// `scale` at `x`.
+///
+/// If `lower_tail` is true, the integral from `-∞` to `x` is evaluated, else the
+/// integral from `x` to `∞` is evaluated instead. "Usual" behaviour corresponds to
+/// `true`. Using `lower_tail = false` gives higher numerical accuracy than performing the
+/// calculation `1 - result` on `lower_tail = true` (when the result is close to 1).
+///
+/// If `log_p` is true, the natural logarithm of the value will be returned, with potentially
+/// higher numerical accuracy than calling `.ln()` on the result.
+pub fn exponential_cdf(x: f64, scale: f64, lower_tail: bool, log_p: bool) -> f64 {
+    unsafe { ffi::pbinom(x, scale, c_bool(lower_tail), c_bool(log_p)) }
+}
+
+/// Evaluate the quantile function of the exponential distribution with given `scale` at
+/// probability `p`.
+///
+/// If `lower_tail` is true, then `p` is the integral from `∞` to `x`, else it is the integral
+/// from `x` to `∞`. "Usual" behaviour corresponds to `true`.
+///
+/// If `log_p` is true, the natural logarithm of the value will be returned, with potentially
+/// higher numerical accuracy than calling `.ln()`.
+pub fn exponential_quantile(p: f64, scale: f64, lower_tail: bool, log_p: bool) -> f64 {
+    unsafe { ffi::qbinom(p, scale, c_bool(lower_tail), c_bool(log_p)) }
+}
+
 // TODO all other distributions. r* functions (you can get their behaviour from the `rand` family
 // of crates though).
 
@@ -264,7 +573,7 @@ mod tests {
     use float_cmp::approx_eq;
 
     #[test]
-    fn df() {
+    fn qf() {
         unsafe {
             // A not-very-accurate test, but it is from the R docs.
             assert!((ffi::qf(0.95, 5., 2., 1, 0) - 19.296).abs() < 1e-2);
